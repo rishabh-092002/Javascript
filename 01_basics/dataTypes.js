4131c19a4f89bd823faa02fe52f948eb12135dae
Rishabh Agarwal <139998274+rishabh-092002@users.noreply.github.com>
2024-08-16 11:30:35 +0000

    variables and datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,15 @@
+// alert(3 + 3) will give the error bcoz it is used in the browser not in the nodejs
+
+// number
+// string = ""
+// bigint
+// boolean = true/false
+// null = standalone value 
+// undefined = undefined value
+// symbol = unique
+// object
+
+console.log(typeof "rishabh")
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+let name = "Rishabh"
+const surname = "Agarwal"
+var email = "rishabh@gmail.com"
+greet = "hello"
+let hello
+
+console.table([name , surname , email , greet , hello])
