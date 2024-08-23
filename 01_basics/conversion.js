ede215fa1a2a0e7b99ce77464027e1073b4dbdcc
Rishabh Agarwal <139998274+rishabh-092002@users.noreply.github.com>
2024-08-23 08:42:44 +0000

    conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,30 @@
+// let name = "33"
+// console.log(typeof(name))
+// let convert = Number(name)
+// console.log(typeof(convert))
+
+// let name = "33aaa"
+// console.log(typeof(name))
+// let convert = Number(name)
+// console.log(typeof(convert))
+// console.log(convert) NaN
+
+// let name = null
+// console.log(typeof(name))
+// let convert = Number(name)
+// console.log(typeof(convert))
+// console.log(convert); 0
+
+// let name = undefined
+// console.log(typeof(name))
+// let convert = Number(name)
+// console.log(typeof(convert))
+// console.log(convert); NaN
+
+let name = true
+console.log(typeof(name))
+let convert = Number(name)
+console.log(typeof(convert))
+console.log(convert);
+
+// Diffrent Conversions : String() , Boolean() , Number()
