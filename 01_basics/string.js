56d8a2e64aa30c223686cbc14a3d3fb6086745fb
Rishabh Agarwal <139998274+rishabh-092002@users.noreply.github.com>
2024-08-23 11:51:04 +0000

    String

---
+++ 01_basics/string.js
@@ -0,0 +1,23 @@
+const name = "rishabh"
+const degree = "B.tech"
+//console.log(name + degree) Old Method to work.
+console.log(`Hello!My name is ${name} and doing ${degree}`);
+
+const name2 = new String("Rishabh")
+
+// console.log(name2[0]);
+// console.log(name2.__proto__);
+// console.log(name2.length);
+// console.log(name2.toUpperCase());
+
+console.log(name2.substring(0,5));
+console.log(name2.slice(-5, 5));
+const name3 = "    hellloooooo    "
+console.log(name3.trim())
+console.log(name3.replace('l' , 'm'));
+console.log(name3.split('l'))
+
+
+
+
+
