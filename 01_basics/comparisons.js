cc091206a3ed0484af1ee2a2d81f27722c4366fb
Rishabh Agarwal <139998274+rishabh-092002@users.noreply.github.com>
2024-08-23 11:12:42 +0000

    datatypessummary

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,16 @@
+// In Javascript equality (==) and comparisons(> < >= etc) works differently.
+// Comparisons convert null into number treating it as 0.
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// console.log("2" === 2); //Strictly Checking Data Type Also
+// console.log("2" == 2);
+
+console.log("2" == 2)
+console.log("021" > 1);
+
--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -9,7 +9,29 @@
 // symbol = unique
 // object
 
-console.log(typeof "rishabh")
-console.log(typeof null) // object
-console.log(typeof undefined) //undefined
+// console.log(typeof "rishabh")
+// console.log(typeof null) // object
+// console.log(typeof undefined) //undefined
 
+/*
+Two Datatypes - Primitives and Non Primitives 
+Primitives(Value type) - String , Number , Boolean , null , undefined , Symbol , BigInt
+cont number = 100.3
+const boolean = false
+let name = null
+let looged; //undefined
+cont name = Symbol("123")
+//Type Of bigint is undefined
+const bigNumber =445545699999884544912n 
+
+Non Primitives(Reference) - Array ,objects , Functions //Type of is "Object" but for function is "object function"
+const numbers = ["one" , "two" , "threww"];
+let myObj = {
+    name: "rishabh",
+    age: 22
+    }
+let myFunction = function(){
+console.log("Hello World")
+}
+
+*/ 
