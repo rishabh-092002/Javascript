498b82ead7034a34bbf9b85184e2ca5d28ee2802
Rishabh Agarwal <139998274+rishabh-092002@users.noreply.github.com>
2024-08-23 12:35:48 +0000

    numbers and maths

---
+++ 01_basics/numbers_maths.js
@@ -0,0 +1,22 @@
+// const number = new Number(400)
+// console.log(number)
+// console.log(number.toString());
+// console.log(number.toFixed(2))
+// const otherNumber = 1000.54565155
+// console.log(otherNumber.toPrecision(4));
+// const number1 = 100000000000000
+// console.log(number1.toLocaleString('en-IN'));
+
+//************Maths***********************/
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(5.8));
+// console.log(Math.round(4.6))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1);
+
+let min = 10 
+let max = 20 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
